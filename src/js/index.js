843b9b159cc0079b45acc2aa23eb2d97726d08d2
Thomas Rzipa <th.rzipa@paprikon.at>
2016-06-28 12:44:11 +0200

    Added some generic classes as a base for my charts

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 /* eslint no-unused-vars: 0 */
 
-import Visualization from './components/Visualization';
+import Visualization from './components/ScatterPlot';
+import config from './config';
 
-const visualization = new Visualization((v) => {
-  console.log('Visualization loaded');
+
+const visualization = new Visualization('.plot', config, v => {
+  v.getData(config.dataUrl, (err, data) => {
+    v.drawGraph();
+  });
 });
